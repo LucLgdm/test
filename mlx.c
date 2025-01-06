8c9c65746e2d9d09695b62216c72aa720b5f191a
Luc De Mercey <LucLgdm@users.noreply.github.com>
2025-01-06 12:30:10 +0100

    forme OK

--- mlx.c
+++ mlx.c
@@ -1,96 +1,69 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   mlx.c                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lde-merc <lde-merc@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/17 06:38:34 by lde-merc          #+#    #+#             */
-/*   Updated: 2025/01/02 15:28:45 by lde-merc         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   mlx.c                                              :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: lde-merc <lde-merc@student.42.fr>          +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2024/12/17 06:38:34 by lde-merc          #+#    #+#             */
+// /*   Updated: 2025/01/06 11:33:05 by lde-merc         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
 #include "data.h"
 #include <stdio.h>
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char *dst;
-	int offset = (y * data->line_length + x * (data->bits_per_pixel / 8));
+int		height, width;
 
-	dst = data->addr + offset;
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color){
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
 
-void	square(t_data *img, int full){
-	if (!full){
-		for(int i = 350; i <= 450; i++){
-			for (int j = 0; j < 6; j++){
-				my_mlx_pixel_put(img, i, 10 + j, 0x00FF0000);
-				my_mlx_pixel_put(img, i, 110 - j, 0x00FF0000);
-				my_mlx_pixel_put(img, 350 + j, i - 340, 0x00FF0000);
-				my_mlx_pixel_put(img, 450 - j, i - 340, 0x00FF0000);
-			}
-		}
-	}else{
-		for(int i = 351; i < 449; i++){
-			for(int j = 11; j < 109; j++){
-				my_mlx_pixel_put(img, i, j, 0x0000FF00);
-			}
+void	square(t_data *img, int origin_x, int origin_y, int largeur){
+	for(int x = 0; x <= largeur; x++){
+		for(int y = 0; y <= largeur; y++){
+			my_mlx_pixel_put(img, origin_x + x, origin_y + y, 0x00FF0000);
 		}
 	}
 }
 
-void circle(t_data *img, int full, int x, int y, float r){
-	if (!full){
-		for(int i = x - r; i < x + r + 1; i++){
-			for(int j = y - r; j < y + r + 1; j++){
-				if (((i-x) * (i-x) + (j-y) * (j-y)) <= (r * r))
-					my_mlx_pixel_put(img, x + i, y + j, 0x00FF0000);
-			}
-		}
-	}else{
-		float ratio = 0.85;
-		for(int i = x - r; i < x + r + 1; i++){
-			for(int j = y - r; j < y + r + 1; j++){
-				if (((i-x) * (i-x) + (j-y) * (j-y)) <= r * r &&
-					((i-x) * (i-x) + (j-y) * (j-y)) >= (ratio * r) * (ratio
-							* r))
-					my_mlx_pixel_put(img, x + i, y + j, 0x0000FF00);
-			}
+void circle(t_data *img, int center_x, int center_y, float r){
+	for(int x = -r; x <= r; x++){
+		for(int y = -r; y <= r; y++){
+			if ((x * x + y * y) <= r * r)
+				my_mlx_pixel_put(img, center_x + x, center_y + y, 0x000000FF);
 		}
 	}
 }
 
-int	main(void)
-{
+int	main(void){
 	void    *mlx;
 	void	*window;
     t_data 	img;
-    int		height, width;
-
+	int		height_w;
+	int		width_w;
+	
     mlx = mlx_init();
-	mlx_get_screen_size(mlx, &height, &width);
-	window = mlx_new_window(mlx, height, width, "so_long");
-    img.img = mlx_new_image(mlx, height, width);
+	mlx_get_screen_size(mlx, &width, &height);
+	height_w = height/2;
+	width_w = width/2;
+	window = mlx_new_window(mlx, width_w, height_w, "so_long");
+    img.img = mlx_new_image(mlx, width_w, height_w);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 			&img.endian);
-	square(&img, 1);
-	square(&img, 0);
 
-	printf("heigth = %i, width = %i\n", height, width);
-	circle(&img, 0, 1750, 300, 200);
-	circle(&img, 1, 1750, 300, 200);
+	printf("heigth = %i, width = %i\nheigth_w = %i, width_w = %i\n", height, width, height_w, width_w);
 
-	circle(&img, 0, 2250, 300, 200);
-	circle(&img, 1, 2250, 300, 200);
+	square(&img, width_w/2 + 150, height_w/2 - 100, 200);
+	square(&img, width_w/2 - 350, height_w/2 - 100, 200);
+	square(&img, width_w/2 - 100, height_w/2 + 150, 200);
+	square(&img, width_w/2 - 100, height_w/2 - 350, 200);
+	circle(&img, width_w/2, height_w/2, 100);
 
-	circle(&img, 0, 2000, 400, 75);
-	circle(&img, 1, 2000, 400, 75);
-
-	circle(&img, 0, 2000, 200, 75);
-	circle(&img, 1, 2000, 200, 75);
 	mlx_put_image_to_window(mlx, window, img.img, 0, 0);
-    mlx_loop(mlx);
+    mlx_loop(mlx);	
 	return (0);
 }

4979c459477c02df0764f289d9a93791b214ca9d
LucLgdm <LucLgdm@users.noreply.github.com>
2025-02-06 15:12:58 +0100

    multi child semaphore

--- semaphore/creation_use_semaphore.c
+++ semaphore/creation_use_semaphore.c
@@ -10,24 +10,37 @@ int main() {
     pid_t pid = fork();
 
     if (pid == 0) {
-        printf("Enfant attend la ressource...\n");
+        printf("Enfant 1 attend la ressource...\n");
         sem_wait(sem);
-        printf("Enfant utilise la ressource\n");
+        printf("Enfant 1 utilise la ressource\n");
         sleep(2);
-        printf("Enfant libère la ressource\n");
+        printf("Enfant 1 libère la ressource\n");
         sem_post(sem);
+		return 0;
     } 
-    else {
-        printf("Parent attend la ressource...\n");
+    
+	pid_t pid2 = fork();
+	if (pid2 == 0){
+		printf("Enfant 2 attend la ressource...\n");
         sem_wait(sem);
-        printf("Parent utilise la ressource\n");
+        printf("Enfant 2 utilise la ressource\n");
         sleep(2);
-        printf("Parent libère la ressource\n");
+        printf("Enfant 2 libère la ressource\n");
         sem_post(sem);
+		return 0;
+	}
 
-        waitpid(pid, NULL, 0);
-        sem_close(sem);
-        sem_unlink("/test_semaphore");
-    }
+	waitpid(pid, NULL, 0);
+	waitpid(pid2, NULL, 0);
+
+	printf("Parent attend la ressource...\n");
+	sem_wait(sem);
+	printf("Parent utilise la ressource\n");
+	sleep(2);
+	printf("Parent libère la ressource\n");
+	sem_post(sem);
+
+	sem_close(sem);
+	sem_unlink("/test_semaphore");
     return 0;
 }

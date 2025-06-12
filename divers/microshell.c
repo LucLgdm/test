8f1823b5cb1d7b59f4ba4fbeb6a2a738ef4d2f79
Luc De Mercey <LucLgdm@users.noreply.github.com>
2025-06-12 14:16:32 +0200

    Cleaning

--- divers/microshell.c
+++ divers/microshell.c
@@ -1,161 +1,66 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#include <signal.h>
 
-
-void print_tab(char **tab) {
-    for (int i = 0; tab[i]; i++) {
-        printf("%s ", tab[i]);
-    }
-    printf("\n");
-}
-
-void execute_pipe(char **cmd, char **next_cmd, char **env){
-    int pipe_fd[2];
-    pid_t pid_1, pid_2;
-
-    if (pipe(pipe_fd) == -1) {
-        perror("error: fatal");
-        exit(1);
-    }
-    
-    pid_1 = fork();
-    if (pid_1 == -1) {
-        perror("error: fatal");
-        exit(1);
-    }
-
-    if (pid_1 == 0) {  // Enfant pour cmd1
-        dup2(pipe_fd[1], STDOUT_FILENO);
-        close(pipe_fd[0]);
-        close(pipe_fd[1]);
-
-        if (execve(cmd[0], cmd, env) == -1){
-            fprintf(stderr, "error: cannot execute %s\n", cmd[0]);
-            exit(1);
-        }
-    }
-
-    pid_2 = fork();
-    if (pid_2 == -1) {
-        perror("error: fatal");
-        exit(1);
-    }
-
-    if (pid_2 == 0) {  // Enfant pour next_cmd
-        dup2(pipe_fd[0], STDIN_FILENO);
-        close(pipe_fd[0]);
-        close(pipe_fd[1]);
-
-        execve(next_cmd[0], next_cmd, env);
-        fprintf(stderr, "error: cannot execute %s\n", next_cmd[0]);
-        exit(1);
-    }
-	close(pipe_fd[0]);
-	close(pipe_fd[1]);
-    
-    waitpid(pid_1, NULL, 0);
-    waitpid(pid_2, NULL, 0);
-    
-    kill(pid_1, SIGKILL);
-    kill(pid_2, SIGKILL);
-}
-
-void execute_command(char **cmd, char **env){
-	if (!cmd || !cmd[0])
-        return;
-
-	if (strcmp(cmd[0], "cd") == 0) {
-		ft_cd(cmd);
-		return;
+void	ft_putstr_fd(char *str, char *arg){
+	while(str && *str)
+		write(2, ++str, 1);
+	if (arg){
+		while(arg)
+			write(2, ++arg, 1);
 	}
-	// Fork and execute the command
-	pid_t pid_1 = fork();
-	if (pid_1 == -1){
-        perror("error: fatal");
-        exit(1);
-    }
+	write(2, "\n", 1);
+}
 
-	if (pid_1 == 0){
-        execve(cmd[0], cmd, env);
-		fprintf(stderr, "error: cannot execute %s\n", cmd[0]);
-        exit(1);
+void	ft_execute(char **argv, char **env, int i, int std_in){
+	argv[i] = NULL;
+	dup2(std_in, STDIN_FILENO);
+	close(std_in);
+	execve(argv[0], argv, env);
+	ft_putstr_fd("error: cannont execute ", argv[0]);
+	exit(1);
+}
+
+int main(int argc, char **argv, char **env){
+	int i = 0;
+	int fd[2];
+	int tmp_fd = dup(STDIN_FILENO);
+	(void)argc;
+
+	while(argv[i] && argv[i + 1]){
+		argv = &argv[i + 1];
+		i = 0;
+		while(argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+			i++;
+		if (strcmp(argv[0], "cd") == 0){
+			if (i != 2)
+				ft_putstr_fd("error: cd: bad arguments\n", NULL);
+			else if (chdir(argv[1]) != 0)
+				ft_putstr_fd("error: cd: cannot change directory to ", argv[1]);
+		}else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0)){
+			if (fork() == 0)
+				ft_execute(argv, env, i, tmp_fd);
+			else {
+				close(tmp_fd);
+				while(waitpid(-1, NULL, WUNTRACED) != -1)
+					;
+				tmp_fd = dup(STDIN_FILENO);
+			}
+		}else if(i != 0 && strcmp(argv[i], "|") == 0){
+			pipe(fd);
+			if (fork() == 0){
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+				ft_execute(argv, env, i, tmp_fd);
+			}else{
+				close(fd[1]);
+				close(tmp_fd);
+				tmp_fd = fd[0];
+			}
+		}
 	}
-
-    waitpid(pid_1, NULL, 0);
-    kill(pid_1, SIGKILL);
-}
-
-char **extract_command(char **argv, int *i) {
-    int start = *i;
-    int count = 0;
-
-    while (argv[*i] && strncmp(argv[*i], ";", 1) != 0 && strncmp(argv[*i], "|", 1) != 0) {
-        (*i)++;
-        count++;
-    }
-
-    char **cmd = malloc((count + 1) * sizeof(char *));
-    if (!cmd)
-        return NULL;
-
-    for (int j = 0; j < count; j++) {
-        cmd[j] = strdup(argv[start + j]);
-        if (!cmd[j]) {
-            while (j > 0)
-                free(cmd[--j]);
-            free(cmd);
-            return NULL;
-        }
-    }
-    cmd[count] = NULL;
-
-    if (argv[*i] && (strncmp(argv[*i], ";", 1) == 0 )) {
-        (*i)++;
-    }
-
-    return cmd;
-}
-
-void free_tab(char **tab) {
-    for (int i = 0; tab[i]; i++)
-        free(tab[i]);
-    free(tab);
-}
-
-
-void ft_cd(char **cmd){
-	if (cmd[1] == NULL){
-		fprintf(stderr, "error: cd: bad arguments\n");
-		return;
-	}
-	if (chdir(cmd[1]) == -1){
-		perror("error: cd");
-	}
-}
-
-int main(int argc, char **argv, char **env) {
-    char **cmd;
-    int i = 1;
-
-    while (i < argc) {
-        cmd = extract_command(argv, &i);
-        if (cmd && *cmd) {
-            if (argv[i] && strcmp(argv[i], "|") == 0) {
-                i++;
-                char **next_cmd = extract_command(argv, &i);
-                if (next_cmd && *next_cmd) {
-                    execute_pipe(cmd, next_cmd, env);
-                }
-                free_tab(next_cmd);
-            } else {
-                execute_command(cmd, env);
-            }
-			free_tab(cmd);
-        }
-    }
-    return 0;
+	close(tmp_fd);
+	return 0;
 }
